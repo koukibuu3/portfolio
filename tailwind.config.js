0d507d9500c4f4027515e78c433f7ea74526a5bd
koukibuu3 <koukibuu3@gmail.com>
2021-12-25 20:11:14 +0900

    chore: tailwindcss v3にアップデート

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  purge: ['./pages/**/*', './components/**/*'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*', './components/**/*'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
