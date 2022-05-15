ef2714e98eb03d473c0f2f8fb382c929a6d3e4fb
koukibuu3 <koukibuu3@gmail.com>
2022-05-15 22:05:20 +0900

    refactor: zennClient.get()をArticlesを返すように修正

--- libs/zennClient.ts
+++ libs/zennClient.ts
@@ -1,13 +1,28 @@
 import Parser from 'rss-parser'
 
-import { ZennFeed, ZennItem } from '../types'
+import { Article, ZennFeed, ZennItem } from '../types'
+
+const fetch = async (): Promise<ZennItem[]> => {
+  const res = await new Parser<ZennFeed, ZennItem>().parseURL(
+    `https://zenn.dev/${process.env.ZENN_USER_ID}/feed`
+  )
+  return res.items
+}
 
 const zennClient = {
-  get: async (): Promise<ZennItem[]> => {
-    const res = await new Parser<ZennFeed, ZennItem>().parseURL(
-      `https://zenn.dev/${process.env.ZENN_USER_ID}/feed`
+  get: async (): Promise<Article[]> => {
+    const zennItems = await fetch()
+    return zennItems.map(
+      (zennItem): Article => ({
+        id: zennItem.link.replace(/^.+articles\//, ''),
+        title: zennItem.title,
+        body: zennItem.content,
+        url: zennItem.link,
+        type: 'zenn',
+        created_at: zennItem.pubDate,
+        updated_at: zennItem.pubDate,
+      })
     )
-    return res.items
   },
 }
 
