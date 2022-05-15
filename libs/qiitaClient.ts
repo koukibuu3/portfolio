7fede2bcd90eeaeeb84bcfce8bb7939a90498ff4
koukibuu3 <koukibuu3@gmail.com>
2022-05-15 22:04:54 +0900

    refactor: qiitaClient.get()をArticlesを返すように修正

--- libs/qiitaClient.ts
+++ libs/qiitaClient.ts
@@ -1,23 +1,34 @@
 import axios from 'axios'
 
-import { QiitaItem } from '../types'
+import { Article, QiitaItem } from '../types'
+
+const fetch = async (): Promise<QiitaItem[]> => {
+  const res = await axios
+    .create({
+      baseURL: `https://qiita.com/api/v2/users/${process.env.QIITA_USER_ID}/`,
+      responseType: 'json',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .get<QiitaItem[]>('items')
+  return res.data
+}
 
 const qiitaClient = {
-  get: async (): Promise<QiitaItem[]> => {
-    const res = await axios
-      .create({
-        baseURL: `https://qiita.com/api/v2/users/${process.env.QIITA_USER_ID}/`,
-        responseType: 'json',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        params: {
-          page: 1,
-          per_page: 5,
-        },
+  get: async (): Promise<Article[]> => {
+    const qiitaItems = await fetch()
+    return qiitaItems.map(
+      (qiitaItem): Article => ({
+        id: qiitaItem.id,
+        title: qiitaItem.title,
+        body: qiitaItem.body,
+        url: qiitaItem.url,
+        type: 'qiita',
+        created_at: qiitaItem.created_at,
+        updated_at: qiitaItem.updated_at,
       })
-      .get<QiitaItem[]>('items')
-    return res.data
+    )
   },
 }
 
