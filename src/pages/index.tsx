fc26ff97d7ce796a2b363d8fa5eff51689dfd171
koukibuu3 <koukibuu3@gmail.com>
2022-06-26 16:56:54 +0900

    refactor: 不要な記述削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,8 @@
-import dayjs from 'dayjs'
 import type { NextPage, GetStaticProps } from 'next'
-import Head from 'next/head'
 
-import {
-  Articles,
-  ContactForm,
-  CustomHead,
-  Footer,
-  Header,
-  Introduce,
-  Pagination,
-  Skills,
-} from '~/components'
-import { client, cmsClient } from '~/libs'
-import { Article, Portfolio, Page } from '~/types'
+import { Articles, CustomHead, Footer, Header, Pagination } from '~/components'
+import { client } from '~/libs'
+import { Article, Page } from '~/types'
 
 const PER_PAGE = 10
 
@@ -29,13 +18,6 @@ const Home: NextPage<Props> = ({ articles, page }) => {
 
       <div className="container text-gray-600 mx-auto my-2">
         <Header />
-        {/* <div className="lg:flex lg:flex-row-reverse">
-          <Introduce
-            portfolio={portfolio}
-            classNames="lg:flex-1 lg:pt-16 m-2"
-          />
-          <Skills skills={portfolio.skills} classNames="lg:flex-1 pt-20 m-2" />
-        </div> */}
         <Articles articles={articles} />
         <Pagination page={page} />
       </div>
