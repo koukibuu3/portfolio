01cf24ea9c0b5e067fc5128efb2f4d7916c2491e
koukibuu3 <koukibuu3@gmail.com>
2024-12-07 01:32:19 +0900

    feat: Knowledgeコンポーネントのデザイン調整 #85

--- src/components/Knowledge/KnowledgeItem.tsx
+++ src/components/Knowledge/KnowledgeItem.tsx
@@ -13,12 +13,14 @@ export const KnowledgeItem: React.FC<Props> = ({ knowledge }) => {
   return (
     <li className="col-span-2 sm:col-span-1" key={knowledge.id}>
       <a href={knowledge.url} target="_blank" rel="noreferrer">
-        <div className="bg-gray-100 hover:bg-gray-200 rounded-lg py-4 px-6">
-          <time className="block h-4 text-gray-500 text-right text-xs my-1">
+        <div className="bg-gray-100 hover:bg-gray-200 rounded-md py-3 px-6">
+          <time className="block h-4 text-right text-xs">
             {dayjs(knowledge.created_at).format('YYYY/MM/DD')}
           </time>
-          <h3 className="text-lg h-14 my-2 line-clamp-2">{knowledge.title}</h3>
-          <div className="flex items-center text-gray-500 text-xs my-1">
+          <h3 className="text-lg font-semibold line-clamp-2 h-14 my-2">
+            {knowledge.title}
+          </h3>
+          <div className="flex items-center text-xs my-1">
             <div className="h-4 w-4">
               <Image
                 src={`/img/${knowledge.type}.png`}
@@ -29,7 +31,7 @@ export const KnowledgeItem: React.FC<Props> = ({ knowledge }) => {
             </div>
             <div className="h-4">{knowledge.type}</div>
             <div className="ml-auto">
-              <FaExternalLinkAlt size="1.3em" />
+              <FaExternalLinkAlt size="14px" />
             </div>
           </div>
         </div>
