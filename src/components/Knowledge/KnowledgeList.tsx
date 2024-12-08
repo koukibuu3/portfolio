648949619dabacc33d4be8ffeac225433c9bb869
koukibuu3 <koukibuu3@gmail.com>
2024-12-08 16:50:00 +0900

    fix: Knowledgeコンポーネントのデザイン調整 #85

--- src/components/Knowledge/KnowledgeList.tsx
+++ src/components/Knowledge/KnowledgeList.tsx
@@ -10,8 +10,11 @@ type Props = {
 
 export const KnowledgeList: React.FC<Props> = ({ knowledgeList }) => {
   return (
-    <section id="knowledge" className="mt-16">
-      <ul className="grid grid-cols-2 gap-4 my-6 lg:mx-5">
+    <section id="knowledge" className="mt-16 mx-4">
+      <h1 className="text-2xl font-semibold">
+        Knowledge<span className="text-sm pl-2">- 技術メモ</span>
+      </h1>
+      <ul className="grid grid-cols-2 gap-3 my-4 lg:mx-4">
         {knowledgeList.map((knowledge) => (
           <KnowledgeItem key={knowledge.id} knowledge={knowledge} />
         ))}
