02f1e9bd51f8b960d97ee5478f972ed5e7c83bad
koukibuu3 <koukibuu3@gmail.com>
2024-12-15 00:43:51 +0900

    wip: Article の dummyデータ作成 ##90

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 
 import { CustomHead, Pagination } from '~/components'
+import { ArticleList } from '~/components/Article/ArticleList'
 import { Copyright } from '~/components/Copyright/Copyright'
 import { GlobalNavigation } from '~/components/GlobalNavigation'
 import { KnowledgeList } from '~/components/Knowledge/KnowledgeList'
@@ -11,15 +12,60 @@ import { Knowledge, Page } from '~/types'
 type Props = {
   knowledgeList: Knowledge[]
   page: Page
+  environment: string
 }
 
-const IndexPage: NextPage<Props> = ({ knowledgeList, page }) => {
+const IndexPage: NextPage<Props> = ({ knowledgeList, page, environment }) => {
+  const articles =
+    environment !== 'production'
+      ? [
+          {
+            id: '1',
+            title: 'とってもかわいいまめちゃんの記事です',
+            description:
+              'まめちゃんはとってもかわいいです。何が可愛いかというととにかくかわいいんです。かわいいったら可愛いんです。だから、かわいいって言ってんでしょうが。かわいいというのですはね、',
+            tags: [
+              { id: '1', name: '猫' },
+              { id: '2', name: '天使' },
+            ],
+            mainImageUrl: '/img/sample-thumbnail.jpg',
+            publishedAt: '2024-12-14',
+          },
+          {
+            id: '2',
+            title: 'とってもかわいいまめちゃんの記事です',
+            description:
+              'まめちゃんはとってもかわいいです。何が可愛いかというととにかくかわいいんです。かわいいったら可愛いんです。だから、かわいいというのですはね、なんだっけかな、えーっと',
+            tags: [],
+            mainImageUrl: '/img/sample-thumbnail.jpg',
+            publishedAt: '2024-12-14',
+          },
+          {
+            id: '3',
+            title: 'とってもかわいいまめちゃんの記事です',
+            description:
+              'まめちゃんはとってもかわいいです。何が可愛いかというととにかくかわいいんです。かわいいったら可愛いんです。だから、かわいいというのですはね、つまるところ、かわいいんです。',
+            tags: [
+              { id: '1', name: '猫' },
+              { id: '2', name: '天使' },
+            ],
+            mainImageUrl: '/img/sample-thumbnail.jpg',
+            publishedAt: '2024-12-14',
+          },
+        ]
+      : []
+
   return (
     <>
       <CustomHead />
 
       <div className="text-gray-600 mx-auto">
         <GlobalNavigation />
+
+        <Section id="article">
+          <SectionTitle title="Article" subTitle="記事" />
+          <ArticleList articles={articles} />
+        </Section>
 
         <Section id="knowledge">
           <SectionTitle title="Knowledge" subTitle="技術メモ" />
