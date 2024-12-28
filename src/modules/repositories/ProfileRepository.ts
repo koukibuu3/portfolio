d0ce5576805b679b7e7b190afdf46c1c53f4f727
koukibuu3 <koukibuu3@gmail.com>
2024-12-28 11:50:29 +0900

    feat: microCMSからプロフィールを取得するリポジトリを作成 #97

---
+++ src/modules/repositories/ProfileRepository.ts
@@ -0,0 +1,18 @@
+import { microCmsClient } from '~/libs'
+import { Profile } from '~/types'
+
+export class ProfileRepository {
+  private readonly client: typeof microCmsClient
+
+  constructor(client: typeof microCmsClient) {
+    this.client = client
+  }
+
+  async get(): Promise<Profile> {
+    const res = await this.client.get<Profile>({
+      endpoint: 'profile',
+    })
+
+    return res
+  }
+}
