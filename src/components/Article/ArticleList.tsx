156b4126163050d3eea94343bbe9329b87abc737
koukibuu3 <koukibuu3@gmail.com>
2025-01-12 16:36:49 +0900

    feat: TOPページをAppRouter移行

--- src/components/Article/ArticleList.tsx
+++ src/components/Article/ArticleList.tsx
@@ -1,23 +1,21 @@
+'use client'
+
 import { ArticleItem } from './ArticleItem'
 
 import { MoreLink } from '~/components/MoreLink'
+import { useArticle } from '~/hooks/useArticle'
 import { Article } from '~/types'
 
 type Props = {
-  articles: Article[]
-  fetchMore: () => void
-  hasMore: boolean
+  defaultArticles: Article[]
 }
 
-export const ArticleList: React.FC<Props> = ({
-  articles,
-  fetchMore,
-  hasMore,
-}) => {
+export const ArticleList: React.FC<Props> = ({ defaultArticles }) => {
+  const { articles, fetchMore, hasMore } = useArticle(defaultArticles)
+
   if (articles.length === 0) {
     return <p className="text-sm mt-6 mx-auto w-32">記事がありません</p>
   }
-
   return (
     <ul className="grid grid-cols-2 gap-10 m-4">
       {articles.map((article) => (
