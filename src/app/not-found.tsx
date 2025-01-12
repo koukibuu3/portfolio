8a0f2fb11f9f8b489c9780c9c2aff41c0b4bf946
koukibuu3 <koukibuu3@gmail.com>
2025-01-12 17:04:07 +0900

    feat: 404ページ作成

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className="text-gray-600 mx-auto max-w-screen-md text-center">
+      <h1 className="text-xl font-bold">404 Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
