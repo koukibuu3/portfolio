b9435dd6994465cea25240b240daec37c745a0a0
koukibuu3 <koukibuu3@gmail.com>
2025-04-10 00:38:08 +0900

    feat: 記事タイトルの前にTECHバッジを暫定で配置（いったんベタ置き）

--- src/components/Article/Title.tsx
+++ src/components/Article/Title.tsx
@@ -3,5 +3,12 @@ type Props = {
 }
 
 export const Title: React.FC<Props> = ({ title }) => {
-  return <h2 className="text-xl font-bold text-black">{title}</h2>
+  return (
+    <h2 className="text-xl font-bold text-black">
+      <span className="bg-blue-500 text-white rounded-full text-sm px-2 py-1 mr-1">
+        TECH
+      </span>
+      {title}
+    </h2>
+  )
 }
