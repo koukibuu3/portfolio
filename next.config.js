c296e24bbab1f7eda2f6e53afdad555b83ab5820
koukibuu3 <koukibuu3@gmail.com>
2025-07-26 18:18:23 +0900

    fix: notesディレクトリの画像参照を有効化

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['images.microcms-assets.io', 'koukibuu3.github.io'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/notes/**',
+      },
+    ],
   },
   pageExtensions: ['ts', 'tsx'],
 }
